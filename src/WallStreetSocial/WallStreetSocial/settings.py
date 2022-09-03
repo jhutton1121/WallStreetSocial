58178364ab6bb1542152c405235d83f0c2a8ca7f
jhutton1121 <74935499+jhutton1121@users.noreply.github.com>
2022-09-03 18:29:43 -0400

    Changed Comment model to RawComment, added pages model w/ homepage_view

--- src/WallStreetSocial/WallStreetSocial/settings.py
+++ src/WallStreetSocial/WallStreetSocial/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #own
     'raw_reddit_comments',
+    'pages',
 ]
 
 MIDDLEWARE = [
--- src/WallStreetSocial/raw_reddit_comments/models.py
+++ src/WallStreetSocial/raw_reddit_comments/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Comment(models.Model):
+class RawComment(models.Model):
     comment_id = models.TextField()
-    comment_timestamp = models.DateField
+    comment_timestamp = models.TextField() # to be changed
     comment_body = models.TextField()
     comment_upvotes = models.IntegerField()
